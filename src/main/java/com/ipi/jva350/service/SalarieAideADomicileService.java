b92d75d0c8c6ef507270e8a7040b4d178cfe4281
xxolletun <153198066+xxolletun@users.noreply.github.com>
2026-02-27 05:05:17 +0100

    Debug Sonar : Add default constructor to SalarieAideADomicileService
    Added a default constructor to SalarieAideADomicileService.

--- src/main/java/com/ipi/jva350/service/SalarieAideADomicileService.java
+++ src/main/java/com/ipi/jva350/service/SalarieAideADomicileService.java
@@ -21,6 +21,7 @@ public class SalarieAideADomicileService {
     private SalarieAideADomicileRepository salarieAideADomicileRepository;
 
     public SalarieAideADomicileService() {
+        // default constructor
     }
 
     /**
